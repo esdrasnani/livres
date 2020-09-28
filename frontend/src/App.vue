0df7e0d1e5ba0006571f2670e564f3d9dfcd26fd
Edrei <edreifernandes@gmail.com>
2020-09-28 17:32:24 -0300

    Criando tela de login e verificando se usuário está logado

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
-    <com-side-bar></com-side-bar>
-    <com-menu></com-menu>
-    <div class="container pt-5 pt-80">
-      <router-view></router-view>
+    <div v-if="estaLogado">
+      <com-side-bar></com-side-bar>
+      <com-menu></com-menu>
+      <div class="container pt-5 pt-80">
+        <router-view></router-view>
+      </div>
+    </div>
+    <div v-else>
+      <app-login v-on:FazerLogin="permitirAcesso($event)" />
     </div>
   </div>
 </template>
@@ -11,11 +16,23 @@
 <script>
 import ComSideBar from "./components/ComSideBar";
 import ComMenu from "./components/ComMenu";
+import Login from "@/components/Login.vue";
 
 export default {
   components: {
     "com-side-bar": ComSideBar,
-    "com-menu": ComMenu
-  }
+    "com-menu": ComMenu,
+    "app-login": Login,
+  },
+  data() {
+    return {
+      estaLogado: false,
+    };
+  },
+  methods: {
+    permitirAcesso(_estaLogado) {
+      this.estaLogado = _estaLogado;
+    },
+  },
 };
 </script>
---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="d-table position-absolute w-100 h-100">
+      <div class="d-table-cell align-middle">
+        <div class="d-flex justify-content-center">
+          <div class="card text-center" style="width: 30rem">
+            <div class="card-header"><h3>Fazer Login</h3></div>
+
+            <div class="card-body">
+              <input
+                type="text"
+                name="username"
+                id="username"
+                class="form-control"
+                placeholder="Usuário"
+                v-model="username"
+              />
+
+              <input
+                type="password"
+                name="senha"
+                id="senha"
+                class="form-control"
+                placeholder="Senha"
+                v-model="senha"
+              />
+            </div>
+
+            <div class="card-footer text-muted">
+              <button
+                type="button"
+                name="login"
+                id="login"
+                class="btn btn-primary"
+                @click="logar()"
+              >
+                Logar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      senha: "",
+    };
+  },
+  methods: {
+    logar() {
+      this.$emit("FazerLogin", true);
+    },
+  },
+};
+</script>
+
+<style></style>
